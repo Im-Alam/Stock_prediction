41a4150c2ce4f4a73b84feb3efea55c90bf48000
Im-Alam <alaimran90@gmail.com>
2024-07-12 02:34:07 +0530

    add cloudinary  upload method

---
+++ src/utils/cloudUpload.py
@@ -0,0 +1,26 @@
+import cloudinary
+import cloudinary.uploader
+import os
+from dotenv import load_dotenv
+import fs
+
+load_dotenv()
+
+# Configuration       
+cloudinary.config( 
+    cloud_name = os.getenv('CLOUDINARY_CLOUD_NAME'), 
+    api_key = os.getenv('CLOUDINARY_API'), 
+    api_secret = os.getenv('CLOUDINARY_SECRET'),
+    secure=True
+)
+
+#Uploading a file on cloudinary
+def uploadOnCloud(localPath:str, public_id = None):
+    try:
+        upload_result = cloudinary.uploader.upload(localPath, public_id = public_id, : 'auto')
+        #now unlink file from local folder
+        return upload_result
+    except Exception as e:
+        #now unlink file from local folder
+        return e
+    
