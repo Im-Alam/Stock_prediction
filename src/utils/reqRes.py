fbf75f0f763977a96a6e9a3edcc05ae6e9eca54e
Im-Alam <alaimran90@gmail.com>
2024-07-12 02:36:48 +0530

    add custom response in flask

---
+++ src/utils/reqRes.py
@@ -0,0 +1,9 @@
+from flask import Response, Request
+
+#Making subclass of api response
+class apiResponse(Response):
+    def __init__(self, statusCode:int, message:str, ):
+        self.status = statusCode
+        self.message = message
+
+Response.        
