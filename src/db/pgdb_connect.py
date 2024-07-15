1d2888a80c00629088ebc2634d7b57ff6287684b
Im-Alam <alaimran90@gmail.com>
2024-07-15 07:22:42 +0530

    return made to engine object instead of connection func

--- src/db/pgdb_connect.py
+++ src/db/pgdb_connect.py
@@ -3,8 +3,11 @@ from sqlalchemy import create_engine
 
 def connectPGDB():
     try:
-        engine = create_engine(os.getenv("PG_URI"))
+        engine = create_engine(os.getenv("AIVEN_PG_URI"))
+        print("Lazy connection setup")
         return engine
     except Exception as e:
-        print("Failed to connect database\n","*"*50,"\n",e)
+        print("Failed to connect database\n","-"*50,"\n",e)
         return 400
+
+engine = connectPGDB()
