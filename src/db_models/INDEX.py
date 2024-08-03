29373f6a247a0dc608d813777b5c195feecc4855
Im-Alam <alaimran90@gmail.com>
2024-08-04 01:10:50 +0530

    added and updatedINDEX data schema

--- src/db_models/INDEX.py
+++ src/db_models/INDEX.py
@@ -9,12 +9,13 @@ from src.utils.reqRes import apiError, apiResponse
 
 class IndicesTable(Base):
     __tablename__ = 'Indices_table'
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    timestamp: Mapped[DateTime] = mapped_column(DateTime, default=func.now(), nullable=False)
-    sensex: Mapped[float] = mapped_column(Float, nullable=False)  # BSE SENSEX (India)
-    nifty50: Mapped[float] = mapped_column(Float, nullable=False)  # NSE NIFTY 50 (India)
+    #collects data of trading day closing price
+
+    timestamp: Mapped[DateTime] = mapped_column(DateTime, default=func.now())
     niftyPrediction: Mapped[float] = mapped_column(Float, nullable=True)  # NSE NIFTY 50 (India)
     predictionAccuracy: Mapped[float] = mapped_column(Float, nullable=True)  # NSE NIFTY 50 (India)
+    nifty50: Mapped[float] = mapped_column(Float, nullable=False)  # NSE NIFTY 50 (India)
+    sensex: Mapped[float] = mapped_column(Float, nullable=False)  # BSE SENSEX (India)  
     bank_nifty: Mapped[float] = mapped_column(Float, nullable=False)  # Bank Nifty (India)
     nifty_auto: Mapped[float] = mapped_column(Float, nullable=False)  # Nifty Auto (India)
     nifty_it: Mapped[float] = mapped_column(Float, nullable=False)  # Nifty IT (India)
@@ -28,11 +29,44 @@ class IndicesTable(Base):
     nikkei_225: Mapped[float] = mapped_column(Float, nullable=False)  # Nikkei 225 (Japan)
     shanghai_composite: Mapped[float] = mapped_column(Float, nullable=False)  # Shanghai Composite (China)
 
+    updated_at: Mapped[DateTime] = mapped_column(DateTime, default=func.now(), onupdate=func.now())
 
     def __repr__(self) -> str:
         return (f"IndicesTable(id={self.id}, timestamp={self.timestamp}")
     
     @classmethod
     def fetch_recent_nData(cls, n):
-        session = Session
-        return session.query(cls).order_by(cls.timestamp.desc()).limit(n).all()
+        session = Session(engine)
+        try:
+            return session.query(cls).order_by(cls.timestamp.desc()).limit(n).all()
+        except Exception as e:
+            session.rollback()
+            return apiError(400, "Error while fetching n index data")
+        finally:
+            session.close()
+
+
+    def insert_data(data_dict: dict):
+        try:
+            session = Session(engine)
+            new_record = IndicesTable(**data_dict)
+            session.add(new_record)
+            session.commit()
+        except Exception as e:
+            session.rollback()
+            return apiError(400, f"Error occured while inserting index data:{e}")
+        finally:
+            session.close()
+
+
+
+    def insert_data_bulk(data_list: list):
+        try:
+            session = Session(engine)
+            session.bulk_insert_mappings(IndicesTable, data_list)
+            session.commit()
+        except Exception as e:
+            session.rollback()
+            return apiError(400, f"bulk insertion into index faules with {e}")
+        finally:
+            session.close()
